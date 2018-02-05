a2f941cacfef460063a98e65a42c676cd1291ec6
US\amidash <amidash@deloitte.com>
2018-02-05 15:10:21 +0530

    Changes to include reactive forms to the app.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,25 +5,27 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages.component'
 import { WebService } from './web.service';
 import { NewMessageComponent } from './new-message.component';
 import { NavComponent } from './navbar.component';
 import { HomeComponent } from './home.component';
+import { RegisterComponent } from './register.component';
 
 var routes = [
               {path: '',component: HomeComponent},
               {path: 'messages',component: MessagesComponent},
-              {path: 'messages/:name',component: MessagesComponent}
+              {path: 'messages/:name',component: MessagesComponent},
+              {path: 'register',component: RegisterComponent}
              ];
 
 @NgModule({
   declarations: [
-    AppComponent, MessagesComponent, NewMessageComponent, NavComponent, HomeComponent],
+    AppComponent, MessagesComponent, NewMessageComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes), BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatCardModule, MatSnackBarModule, HttpModule, MatInputModule, MatFormFieldModule, FormsModule
+    BrowserModule, RouterModule.forRoot(routes), BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatCardModule, MatSnackBarModule, HttpModule, MatInputModule, MatFormFieldModule, FormsModule, ReactiveFormsModule
   ],
   providers: [WebService],
   bootstrap: [AppComponent]
